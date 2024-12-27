f311edcd48dd7d6374d5335366bff9abbfb58ade
Luis2104 <fernandoguerrachipana@gmail.com>
2024-12-27 16:15:16 -0500

    Backend version final 0.2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get()
-  getHello(): string {
+  @Get('api') // Responde a la ruta /api
+  getApi(): string {
     return '¡Conexión exitosa!';
   }
 }
